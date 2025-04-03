2e817ac6b3c460658e2773d312ad023fa791aef7
BOXI CHEN <zxc99361x@gmail.com>
2025-04-03 10:45:03 +0800

    book_1

---
+++ new_create_model/m-1.py
@@ -0,0 +1,7 @@
+def add(n1,n2):
+    return n1+n2
+def sub (n1,n2):
+    return n1-n2
+    
+print(add(5,2))
+print(sub(5,2))
---
+++ new_create_model/m-2.py
@@ -0,0 +1,4 @@
+import calculate 
+
+print(calculate.add(5,2))
+print(calculate.sub(5,2))
