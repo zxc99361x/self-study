2fd3bca4a84d69a87d068b3d39c78d393aba76e7
BOXI CHEN <zxc99361x@gmail.com>
2025-04-04 11:09:41 +0800

    book_2

---
+++ filer3.py
@@ -0,0 +1,3 @@
+with open('file1.txt','r',encoding="utf-8") as f:
+    str1=f.read(5)
+    print(str1)
---
+++ filer4.py
@@ -0,0 +1,4 @@
+with open('file2.txt','r',encoding='utf-8') as f:
+    print(f.readline())
+    print(f.readline(3))
+    
