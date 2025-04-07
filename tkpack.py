a6e3e6de2447b2cdf0a2aee7dc7cabd26d5ad442
BOXI CHEN <zxc99361x@gmail.com>
2025-04-07 11:07:08 +0800

    book_3

---
+++ tkpack.py
@@ -0,0 +1,12 @@
+import tkinter as tk 
+win=tk.Tk()
+
+but1=tk.Button(win,text="按鈕一",width=20)
+but1.pack()
+but2=tk.Button(win,text="按鈕二",width=20)
+but2.pack()
+but3=tk.Button(win,text="按鈕三",width=20)
+but3.pack()
+but4=tk.Button(win,text="按鈕四",width=20)
+but4.pack()
+win.mainloop()
