99cc3a8e96d1caacda70f0736abae917bdb6b5f1
BOXI CHEN <zxc99361x@gmail.com>
2025-04-07 11:36:06 +0800

    book_3

---
+++ tkgrid2.py
@@ -0,0 +1,17 @@
+import tkinter as tk 
+win=tk.Tk()
+
+
+but1=tk.Button(win,text="按鈕一",width=20)
+but1.grid(row=0,column=0,padx=20,pady=5)
+but2=tk.Button(win,text="按鈕二",width=20)
+but2.grid(row=0,column=1,padx=20,pady=5,columnspan=50,sticky="E")#與書上圖片所呈現不同 
+but3=tk.Button(win,text="按鈕三",width=20)
+but3.grid(row=0,column=2,padx=20,pady=5)
+but4=tk.Button(win,text="按鈕四",width=20)
+but4.grid(row=1,column=0,padx=20,pady=5)
+but5=tk.Button(win,text="按鈕五",width=20)
+but5.grid(row=1,column=1,padx=20,pady=5)
+but6=tk.Button(win,text="按鈕六",width=20)
+but6.grid(row=1,column=2,padx=20,pady=5)
+win.mainloop()
